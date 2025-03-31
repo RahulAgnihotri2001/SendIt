325a536df0df64b0a252ba197ec1c2adc1ad6508
Rahul Agnihotri <rahulagnihotri4444@gmail.com>
2025-03-31 15:30:54 +0530

    Auth service

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,7 +1,34 @@
+import User from "../models/user.model.js"
+import bcrypt from "bcryptjs"
+export const signup = async(req,res)=>{
+    const {fullName,email,password}=req.body;  
+    try{
+       if(password.length<6){
+        return res.status(400).json({message:"Password must be at least 6 characters"});
+       }
+       const user = await User.findOne({email});
+       if(user){
+        return res.status(400).json({message:"Email already exist"});
+       }
+       
+       const salt = await bcrypt.genSalt(10);
+       const hashedPassword = await bcrypt.hash(password,salt);
+       const newUser = new User({
+        fullName,
+        email,
+        password:hashedPassword
+       });
 
-export const signup = (req,res)=>{
-    res.send("Signup route");
-}
+       if(newUser){
+        // Generate JWT token
+       }
+       else{
+        return res.status(400).json({message:"Invalid User data"});
+       }
+    } catch(error){
+
+    }
+};
 
 export const login = (req,res)=>{
     res.send("Login route");
--- backend/src/index.js
+++ backend/src/index.js
@@ -1,10 +1,16 @@
 
 import express from "express"
 import authRoutes from "./routes/auth.route.js"
+import dotenv from "dotenv"
+import { connectDB } from "./lib/db.js";
 const app =express();
+app.use(express.json());  // To extract the Json data out of body
+dotenv.config()
+const PORT = process.env.PORT;
 
 app.use('/api/auth',authRoutes)
 
-app.listen(5001,()=>{
-    console.log('Server running on port 5001');
+app.listen(PORT,()=>{
+    console.log("Server running on port : "+ PORT);
+    connectDB()
 })
